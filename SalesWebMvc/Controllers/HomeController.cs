73e03924d8c0afe1c76c7ea12137d8b98802ff90
N41LL1W <willian.play.cel@gmail.com>
2023-11-03 11:25:22 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -18,6 +18,14 @@ namespace SalesWebMvc.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Salles Web MVC App from C# Course";
+            ViewData["Professor"] = "Nelio Alves";
+
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
